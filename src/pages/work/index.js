b04b091b0167f716e898d08ee2c4db3b9c70f94c
Ralph J McIntyre <ralph.j.mcintyre@gmail.com>
2020-06-20 14:12:58 -0400

    update title.

--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -22,7 +22,7 @@ export default class WorkIndexPage extends React.Component {
               padding: '1rem',
             }}
           >
-            Latest Stories
+            Work
           </h1>
         </div>
         <section className="section">
