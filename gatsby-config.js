07516d30ecba200b4cc930a12a563d828eefa5dc
Ralph J McIntyre <ralph.j.mcintyre@gmail.com>
2020-08-23 16:48:32 -0400

    Fix fonts.

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,15 +60,6 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-web-font-loader",
-      options: {
-        custom: {
-          families: ["b612regular"],
-          urls: ["/fonts/fonts.css"],
-        },
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // replace "UA-XXXXXXXXX-X" with your own Tracking ID
